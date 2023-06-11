7d9ac42e7200475265dcbf50e7782d9da2d50c95
Anatoly <topplang1337@mail.ru>
2023-06-11 19:19:24 +0300

    fixes

--- views/questions.py
+++ views/questions.py
@@ -17,8 +17,17 @@ class QuestionsView(Resource):
 
     def post(self):
         data = request.json
+        questions_count = data.get("questions_num", None)
+        if len(data) == 1:
+            if not questions_count:
+                return {"msg": "Request bad format"}, 401
+            elif not isinstance(questions_count, int):
+                return {"msg": "Bad type of value"}, 401
+        else:
+            return {"msg": "Count of values not exact"}, 401
+
         last_question = questions_service.get_last_question()
-        questions = questions_service.get_questions_from_external_api(count=data["questions_num"])
+        questions = questions_service.get_questions_from_external_api(count=questions_count)
 
         questions_service.iteration_by_questions(questions)
         if last_question:
